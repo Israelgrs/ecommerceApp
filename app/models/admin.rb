ccfec69bb2c55fb80a3a8d0de121f1960f9c078c
Israelgrs <israel_grs@live.com>
2022-08-17 22:33:27 -0300

    fixing scopes

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,11 +1,16 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # Scopes
   scope :with_full_access, -> { where(role: :full_access) }
+  scope :with_restricted_access, -> { where(role: :restricted_access) }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :trackable
 
+  # Enums
   enum role: %i[full_access restricted_access]
 
   def full_name
