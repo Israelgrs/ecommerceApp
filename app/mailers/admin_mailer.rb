232d5ab6e48e5100b43cad71ea1f0f76c3c12791
Israelgrs <israel_grs@live.com>
2022-10-27 23:37:49 -0300

    send_email action in ApplicationMailer class and soft refactor in edit.js

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -5,4 +5,11 @@ class AdminMailer < ApplicationMailer
     @admin = admin
     mail(to: @admin.email, subject: 'Seus dados foram alterados!')
   end
+
+  def send_email(current_admin, recipient, subject)
+    @current_admin = current_admin
+    @recipient = recipient
+    @subject = subject
+    mail(to: @recipient.email, subject: @subject.to_s)
+  end
 end
