baba6bf8367a6f709c001f97201dceeecb24c6aa
Israelgrs <israel_grs@live.com>
2022-11-07 23:49:14 -0300

    attached files for Ad model

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -2,6 +2,9 @@ class Ad < ApplicationRecord
   belongs_to :category
   belongs_to :member
 
+  # Active storage image
+  has_one_attached :picture
+
   # gem money_rails 
   monetize :price_cents
 end
