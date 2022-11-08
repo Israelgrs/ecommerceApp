cad27de113082ef56a18c969504a3a8115e107c8
Israelgrs <israel_grs@live.com>
2022-11-08 00:25:09 -0300

    attached files for Ad model

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -3,7 +3,9 @@ class Ad < ApplicationRecord
   belongs_to :member
 
   # Active storage image
-  has_one_attached :picture
+  has_one_attached :picture do |attached|
+    attached.variant :thumb, resize_to_limit: [100, 100]
+  end
 
   # gem money_rails 
   monetize :price_cents
