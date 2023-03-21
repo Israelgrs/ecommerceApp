97830447f4ca935b5ac557355ff26c9d003d0092
Israelgrs <israel_grs@live.com>
2023-03-20 22:37:42 -0300

    reset passsword layout and smtp for mailcatcher at development environment

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,6 +5,9 @@ class Member < ApplicationRecord
   # Associations
   has_many :ads
 
+  validates_presence_of :first_name, message: 'não pode ficar em branco'
+  validates_presence_of :last_name, message: 'não pode ficar em branco'
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,8 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # Letter Opener
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
   config.action_mailer.perform_deliveries = true
   # Devise config
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
